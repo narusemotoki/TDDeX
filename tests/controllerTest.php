bc9d7681ac8354b0fd1c49883af173e8980a013a
kseta <keigo.seta@gmail.com>
2013-07-28 16:44:38 +0900

    不要なファイルを削除

--- tests/controllerTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-class ControllerTest extends PHPUnit_Framework_TestCase
-{
-    public function setup()
-    {
-
-    }
-}
---
+++ tests/todoServiceTest.php
@@ -0,0 +1,15 @@
+<?php
+require_once "./src/todoService.php";
+
+class todoServiceTest extends PHPUnit_Framework_TestCase
+{
+    public function setup()
+    {
+
+    }
+
+    public function test()
+    {
+
+    }
+}
